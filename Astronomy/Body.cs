1f54429d58e13970109c7670c0f70dca6d3eadc9
Azat Salakhutdinov <grei-wtf@mail.ru>
2021-04-09 17:44:29 +0300

    Change the access modifier  and add summary

--- Astronomy/Body.cs
+++ Astronomy/Body.cs
@@ -4,30 +4,69 @@ namespace Astronomy
 {
     public class Body
     {
+        /// <summary>
+        /// Sunrise time
+        /// </summary>
         public DateTime Sunrise { get; internal set; }
 
+        /// <summary>
+        /// Sunset time
+        /// </summary>
         public DateTime Sunset { get; internal set; }
 
+        /// <summary>
+        /// Sunrise end time
+        /// </summary>
         public DateTime SunriseEnd { get; internal set; }
 
+        /// <summary>
+        /// Sunset start time
+        /// </summary>
         public DateTime SunsetStart { get; internal set; }
 
+        /// <summary>
+        /// Dawn time
+        /// </summary>
         public DateTime Dawn { get; internal set; }
 
+        /// <summary>
+        /// Dusk time
+        /// </summary>
         public DateTime Dusk { get; internal set; }
 
+        /// <summary>
+        /// The beginning of the evening astronomical twilight 
+        /// </summary>
         public DateTime NauticalDawn { get; internal set; }
 
+        /// <summary>
+        /// Early morning nautical twilight 
+        /// </summary>
         public DateTime NauticalDusk { get; internal set; }
 
+        /// <summary>
+        /// End time of the night
+        /// </summary>
         public DateTime NightEnd { get; internal set; }
 
+        /// <summary>
+        /// Start time of the night
+        /// </summary>
         public DateTime Night { get; internal set; }
 
+        /// <summary>
+        /// End of soft light, the most suitable time for photography 
+        /// </summary>
         public DateTime GoldenHourEnd { get; internal set; }
 
+        /// <summary>
+        /// Start of soft light, the most suitable time for photography 
+        /// </summary>
         public DateTime GoldenHour { get; internal set; }
 
+        /// <summary>
+        /// The sun is at its highest point 
+        /// </summary>
         public DateTime SolarNoon { get; internal set; }
 
         public DateTime Nadir { get; internal set; }
--- Astronomy/Calculator.cs
+++ Astronomy/Calculator.cs
@@ -6,9 +6,9 @@ namespace Astronomy
 {
     public class Calculator
     {
-        public static readonly double rad = Math.PI / 180;
+        internal static readonly double rad = Math.PI / 180;
 
-        static readonly double dayMs = 1000 * 60 * 60 * 24,
+        internal static readonly double dayMs = 1000 * 60 * 60 * 24,
                J1970 = 2440588,
                J2000 = 2451545;
 
@@ -16,59 +16,57 @@ namespace Astronomy
 
         // general calculations for position
 
-        static readonly double e = rad * 23.4397; // obliquity of the Earth
+        internal static readonly double e = rad * 23.4397; // obliquity of the Earth
 
-        static readonly Func<double, double, double> RightAscension = (double l, double b) => Math.Atan2(Math.Asin(l) * Math.Cos(e) - Math.Tan(b) * Math.Asin(e), Math.Cos(l));
+        internal static readonly Func<double, double, double> RightAscension = (double l, double b) => Math.Atan2(Math.Asin(l) * Math.Cos(e) - Math.Tan(b) * Math.Asin(e), Math.Cos(l));
 
-        static readonly Func<double, double, double> Declination = (double l, double b) => Math.Asin(Math.Sin(b) * Math.Cos(e) + Math.Cos(b) * Math.Sin(e) * Math.Sin(l));
+        internal static readonly Func<double, double, double> Declination = (double l, double b) => Math.Asin(Math.Sin(b) * Math.Cos(e) + Math.Cos(b) * Math.Sin(e) * Math.Sin(l));
 
-        static readonly Func<double, double, double, double> Azimuth = (double H, double phi, double dec) => Math.Atan2(Math.Sin(H), Math.Cos(H) * Math.Sin(phi) - Math.Tan(dec) * Math.Cos(phi));
+        internal static readonly Func<double, double, double, double> Azimuth = (double H, double phi, double dec) => Math.Atan2(Math.Sin(H), Math.Cos(H) * Math.Sin(phi) - Math.Tan(dec) * Math.Cos(phi));
 
-        static readonly Func<double, double, double, double> Altitude = (double H, double phi, double dec) => Math.Asin(Math.Sin(phi) * Math.Sin(dec) + Math.Cos(phi) * Math.Cos(dec) * Math.Cos(H));
+        internal static readonly Func<double, double, double, double> Altitude = (double H, double phi, double dec) => Math.Asin(Math.Sin(phi) * Math.Sin(dec) + Math.Cos(phi) * Math.Cos(dec) * Math.Cos(H));
 
-        static readonly Func<double, double, double> SiderealTime = (double d, double lw) => rad * (280.16 + 360.9856235 * d) - lw;
+        internal static readonly Func<double, double, double> SiderealTime = (double d, double lw) => rad * (280.16 + 360.9856235 * d) - lw;
 
-        static readonly Func<double, double> AstroRefraction = (double h) =>
+        internal static readonly Func<double, double> AstroRefraction = (double h) =>
         {
             if (h < 0) // the following formula works for positive altitudes only.
                 h = 0; // if h = -0.08901179 a div/0 would occur.
 
-            // formula 16.4 of "Astronomical Algorithms" 2nd edition by Jean Meeus (Willmann-Bell, Richmond) 1998.
-            // 1.02 / tan(h + 10.26 / (h + 5.10)) h in degrees, result in arc minutes -> converted to rad:
             return 0.0002967 / Math.Tan(h + 0.00312536 / (h + 0.08901179));
         };
 
         #endregion
 
-        static readonly Func<DateTime, double> ToJulian = (DateTime date) => new DateTimeOffset(date).ToUnixTimeMilliseconds() / dayMs - 0.5 + J1970;
+        internal static readonly Func<DateTime, double> ToJulian = (DateTime date) => new DateTimeOffset(date).ToUnixTimeMilliseconds() / dayMs - 0.5 + J1970;
 
         //static readonly Func<double, DateTime> FromJulian = (double j) => new DateTime((long)(((j + 0.5 - J1970)* 10000) * dayMs),DateTimeKind.Utc);
 
-        static readonly Func<double, DateTime> FromJulian = (double j) => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds((j + 0.5 - J1970) * dayMs).ToLocalTime();
+        internal static readonly Func<double, DateTime> FromJulian = (double j) => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds((j + 0.5 - J1970) * dayMs).ToLocalTime();
 
-        public static readonly Func<DateTime, double> ToDays = (DateTime date) => ToJulian(date) - J2000;
+        internal static readonly Func<DateTime, double> ToDays = (DateTime date) => ToJulian(date) - J2000;
 
         #region calculations for sun times
 
-        static readonly double J0 = 0.0009;
+        internal static readonly double J0 = 0.0009;
 
-        static readonly Func<double, double, double> JulianCycle = (double d, double lw) => Math.Round(d - J0 - lw / (2 * Math.PI));
+        internal static readonly Func<double, double, double> JulianCycle = (double d, double lw) => Math.Round(d - J0 - lw / (2 * Math.PI));
 
-        static readonly Func<double, double, double, double> ApproxTransit = (double ht, double lw, double n) => J0 + (ht + lw) / (2 * Math.PI) + n;
+        internal static readonly Func<double, double, double, double> ApproxTransit = (double ht, double lw, double n) => J0 + (ht + lw) / (2 * Math.PI) + n;
 
-        static readonly Func<double, double, double, double> SolarTransitJ = (double ds, double M, double L) => J2000 + ds + 0.0053 * Math.Sin(M) - 0.0069 * Math.Sin(2 * L);
+        internal static readonly Func<double, double, double, double> SolarTransitJ = (double ds, double M, double L) => J2000 + ds + 0.0053 * Math.Sin(M) - 0.0069 * Math.Sin(2 * L);
 
-        static readonly Func<double, double, double, double> HourAngle = (double h, double phi, double d) => Math.Acos((Math.Sin(h) - Math.Sin(phi) * Math.Sin(d)) / (Math.Cos(phi) * Math.Cos(d)));
+        internal static readonly Func<double, double, double, double> HourAngle = (double h, double phi, double d) => Math.Acos((Math.Sin(h) - Math.Sin(phi) * Math.Sin(d)) / (Math.Cos(phi) * Math.Cos(d)));
 
-        static readonly Func<double, double> ObserverAngle = (double height) => -2.076 * Math.Sqrt(height) / 60;
+        internal static readonly Func<double, double> ObserverAngle = (double height) => -2.076 * Math.Sqrt(height) / 60;
 
         #endregion
 
         #region general sun calculations
 
-        static readonly Func<double, double> SolarMeanAnomaly = (double d) => rad * (357.5291 + 0.98560028 * d);
+        internal static readonly Func<double, double> SolarMeanAnomaly = (double d) => rad * (357.5291 + 0.98560028 * d);
 
-        static readonly Func<double, double> EclipticLongitude = (double M) =>
+        internal static readonly Func<double, double> EclipticLongitude = (double M) =>
         {
             double C = rad * (1.9148 * Math.Sin(M) + 0.02 * Math.Sin(2 * M) + 0.0003 * Math.Sin(3 * M)), // equation of center
                 P = rad * 102.9372; // perihelion of the Earth
@@ -79,7 +77,7 @@ namespace Astronomy
         #endregion
 
         // returns set time for the given sun altitude
-        static double GetSetJ(double h, double lw, double phi, double dec, double n, double M, double L)
+        internal static double GetSetJ(double h, double lw, double phi, double dec, double n, double M, double L)
         {
 
             double w = HourAngle(h, phi, dec),
@@ -87,7 +85,7 @@ namespace Astronomy
             return SolarTransitJ(a, M, L);
         }
 
-        static Tuple<DateTime, DateTime> GetTimesCalculate(double constant, double lw, double phi, double dec, double n, double M, double L, double dh, double Jnoon)
+        internal static Tuple<DateTime, DateTime> GetTimesCalculate(double constant, double lw, double phi, double dec, double n, double M, double L, double dh, double Jnoon)
         {
             double h0 = (constant + dh) * rad;
 
@@ -97,6 +95,14 @@ namespace Astronomy
             return new Tuple<DateTime, DateTime>(FromJulian(Jrise), FromJulian(Jset));
         }
 
+        ///<summary>
+        ///Get Sun Times
+        ///</summary>
+        ///<param name="date">Date and time for calculation</param>
+        ///<param name="lat">Latitude</param>
+        ///<param name="lng">Longtitude</param>
+        ///<param name="height">Optional height</param>
+        ///<returns>A Body class with sun times</returns>
         static public Body GetTimes(DateTime date, double lat, double lng, double height = 0)
         {
             double lw = rad * -lng,
