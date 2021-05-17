659ff10d9215d1bce8867b6a9b8896619c3a8945
SalakhutdinovA <grei-wtf@mail.ru>
2021-05-17 22:05:44 +0300

    add summary

--- Astronomy/BaseCalculator.cs
+++ Astronomy/BaseCalculator.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Astronomy
 {
+    /// <summary>
+    /// Base method for calculation
+    /// </summary>
     public class BaseCalculator
     {
         internal static readonly double rad = Math.PI / 180;
